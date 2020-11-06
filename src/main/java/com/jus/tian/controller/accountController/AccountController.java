90ed3de352dfcd9a9fc087b0e14808154df499ee
14734 <1473441767@qq.com>
2020-11-06 10:44:26 +0800

    redirect and transactionManager

--- src/main/java/com/jus/tian/controller/accountController/AccountController.java
+++ src/main/java/com/jus/tian/controller/accountController/AccountController.java
@@ -5,18 +5,19 @@ import com.jus.tian.service.accountService.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 /**
  * @Auther: JusChui
  * @Date: 2020/11/5 15:20
- * @Description:
- * Account的Controller层
+ * @Description: Account的Controller层
  */
 @Controller
 @RequestMapping("/account")
@@ -26,18 +27,19 @@ public class AccountController {
     private IAccountService accountService;
 
     @RequestMapping(path = "/findAll")
-    public String findAll(Model model){
+    public String findAll(Model model) {
         System.out.println("表现层Controller执行...");
         //调用service的findAll方法
         List<Account> accounts = accountService.findAll();
-        model.addAttribute("list",accounts);
+        model.addAttribute("list", accounts);
         return "list";
     }
 
     @RequestMapping(path = "/saveAccount", method = {RequestMethod.POST})
-    public String saveAccount(Account account){
-        account.setId(UUID.randomUUID().toString().replace("-",""));
+    public String saveAccount(Account account) throws IOException {
+        account.setId(UUID.randomUUID().toString().replace("-", ""));
         accountService.saveAccount(account);
-        return "success";
+//        response.sendRedirect(request.getContextPath() + "/account/findAll");
+        return "redirect:/account/findAll";
     }
 }
